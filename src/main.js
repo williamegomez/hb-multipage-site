3ee4f12e94a68d825cd87a70782fb74e5dcf8c09
William Gomez <william.gomex@gmail.com>
2018-02-08 21:13:36 -0500

    Loading different images depending on the viewport.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import MainMenu from './modules/MainMenu/MainMenu'
+import MainHero from './modules/MainHero/MainHero'
 
 /* eslint-disable */
 const menu = new MainMenu(document.querySelector('.js-MainMenu-1'))
+const hero = new MainHero(document.querySelector('.js-MainHero-1'))
