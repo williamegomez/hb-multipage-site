956a4c2ac0a6da183e7aca9db5eefc0e03515b20
William Gomez <william.gomex@gmail.com>
2018-02-13 21:12:06 -0500

    Using background-image to set the source, but image of medium and large size are loaded using js and modifiers.

--- src/modules/HeroCss/HeroCss.js
+++ src/modules/HeroCss/HeroCss.js
@@ -5,6 +5,18 @@ import jsonHero from './../../../data/HeroCss.json'
 export default class HeroCss {
   constructor (node) {
     this.node = node
+    this.screenWidth = window.screen.width
     this.node.innerHTML = template(jsonHero)
+    this.image = node.querySelector('.hero-css__img')
+    this.setSizeImage()
+  }
+
+  setSizeImage () {
+    if ((this.screenWidth > 639) & (this.screenWidth < 1024)) {
+      this.image.classList.add('hero-css__img--medium')
+    }
+    if (this.screenWidth > 1024) {
+      this.image.classList.add('hero-css__img--large')
+    }
   }
 }
