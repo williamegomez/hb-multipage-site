e1c21ba02adf0877f7966ecd1466b1a8eeb6f04a
William Gomez <william.gomex@gmail.com>
2018-02-14 11:37:25 -0500

    Responsive utility to manage the screen size with  a function that checks if is within the size required

--- src/modules/HeroCss/HeroCss.js
+++ src/modules/HeroCss/HeroCss.js
@@ -1,22 +1,23 @@
 import './HeroCss.scss'
 import template from './HeroCss.pug'
 import jsonHero from './../../../data/HeroCss.json'
+import ResponsiveUtility from './../shared/ResponsiveUtility'
 
 export default class HeroCss {
   constructor (node) {
     this.node = node
-    this.screenWidth = window.screen.width
     this.node.innerHTML = template(jsonHero)
+    this.responseUtil = new ResponsiveUtility()
     this.image = node.querySelector('.hero-css__img')
     this.image.style.backgroundImage = `url(${jsonHero.images.small.url})`
     this.setSizeImage()
   }
 
   setSizeImage () {
-    if ((this.screenWidth > 639) & (this.screenWidth < 1024)) {
+    if (this.responseUtil.checkScreenWidth('medium')) {
       this.image.style.backgroundImage = `url(${jsonHero.images.medium.url})`
     }
-    if (this.screenWidth > 1024) {
+    if (this.responseUtil.checkScreenWidth('large')) {
       this.image.style.backgroundImage = `url(${jsonHero.images.large.url})`
     }
   }
